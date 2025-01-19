60e8a2c4a604a75434c10b041a073058e1579e4e
bilgin20 <kcnblgn@gmail.com>
2025-01-19 11:53:11 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7276130468:AAGNip3eGBJvGaXKBVByBfPUtlCLOWLrKN4";
+    $apiToken = "7150415313:AAFJPMMUqZjxcquV_-IFCHVcySTWNs5T1ZQ";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
