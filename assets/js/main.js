e9b954324e70d8d871de1d24cf7176c153dfd2f3
bilgin20 <kcnblgn@gmail.com>
2025-01-30 14:13:50 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -129,3 +129,24 @@ function getDailyReward() {
 }
 
 rewardBtn.addEventListener('click', getDailyReward);
+//touset wallet
+const btn = document.querySelector("#connectBtn");
+const addressCont = document.querySelector("#addressCont");
+
+const connect = async () => {
+  // console.log("Clicked");
+  // console.log(typeof window.ethereum);
+  if (typeof window.ethereum !== "undefined") {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    }); //REQUEST ACCESS TO USER'S WALLET
+    // console.log(accounts);
+    btn.textContent = "Connected!";
+    const address = accounts[0];
+    addressCont.textContent = address;
+  } else {
+    addressCont.textContent = "Please Install Trust Wallet or her kids.";
+  }
+};
+// EVENT LISTENERS
+btn.addEventListener("click", connect);
